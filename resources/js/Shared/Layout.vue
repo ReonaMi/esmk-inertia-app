99d9bf54d25d39b4df6c689ce95a61d037a0d0f2
ReonaMi <reonamimimi@gmail.com>
2021-04-02 08:47:18 +0700

    init inertia server side & client side

---
+++ resources/js/Shared/Layout.vue
@@ -0,0 +1,21 @@
+<template>
+    <main>
+        <slot/>
+    </main>
+</template>
+
+<script>
+export default {
+    props: {
+        title: String,
+    },
+    watch: {
+        title: {
+            immediate: true,
+            handler(title){
+                document.title = title
+            }
+        }
+    }
+}
+</script>
